d20e2d8a9fc4e7433cd3e32c8da108b0c2022ec2
Nishchit Bhandari <nishchitbhandari2061@gmail.com>
2025-01-02 00:00:10 +0545

    modify prompt to remove markdown components

--- prompts.py
+++ prompts.py
@@ -8,7 +8,6 @@ ALWAYS follow the datatype of arguments as mentioned in tool's descriptions.
 You MUST follow all these steps without missing a single steps.
 To use a tool, please use the following format:
 
-```
 Question: the input question you must answer
 Thought: you should always think about what to do
 Tool Thought: Do I need to use a tool? Yes
@@ -18,14 +17,13 @@ Observation: the result of the action
 ... (this Thought/Tool Thought/Action/Action Input/Observation can repeat N times, but don't repeat infinitely)
 Thought: I now know the final answer
 Final Answer: the final answer to the original input question.
-```
+
 
 When you have a response to say to the Human, or if you do not need to use a tool, you MUST use the format:
 
-```
 Thought: Do I need to use a tool? No
 Final Answer: [your response here]
-```
+
 NOTE: STRICTLY Don't use markdown components like ``` or any other components anywhere.
 Begin!
 
