7ff8ffbf16f999eed82d080bf007a98a06e48047
Nishchit Bhandari <nishchitbhandari2061@gmail.com>
2025-01-06 14:15:14 +0545

    Remove GLHF integration as they discontinued free API

--- model_manager.py
+++ model_manager.py
@@ -8,36 +8,14 @@ from prompts import agent_prompt
 # Importing agent creation and execution modules
 from langchain.agents import AgentExecutor, create_react_agent
 import os
-from colorama import Fore, Style
 # Importing dotenv for environment variables
 from dotenv import load_dotenv
 load_dotenv()
 
-
-# Initialize the ChatOpenAI model with API key and configurations
-
-
-models_info = [
-
-    {"model": GoogleGenerativeAI(
+llm = GoogleGenerativeAI(
         google_api_key=os.getenv("GOOGLE_GEMINI_API"),
         model="gemini-2.0-flash-exp"
-    ), "name": "Gemini-2.0-Flash-Exp"},
-    {"model": ChatOpenAI(
-        api_key=os.getenv("GLHF_API_KEY"),
-        base_url="https://glhf.chat/api/openai/v1",
-        model="hf:meta-llama/Llama-3.3-70B-Instruct",
-    ), "name": "Llama-3.3-70B-Instruct"},
-
-]
-models = [model["model"] for model in models_info]
-print(f"{Fore.GREEN}Available models: {Style.RESET_ALL}")
-for index, model in enumerate(models_info):
-    print(f"{index}: {model["name"]}")
-model_index = int(
-    input(f"Choose your model {Fore.CYAN}0-{index}:{Style.RESET_ALL} "))
-
-llm = models[model_index]
+    )
 
 # Load the react prompt from langchain hub and set custom template
 react_docstore_prompt = hub.pull("hwchase17/react-chat")
--- tools.py
+++ tools.py
@@ -15,8 +15,11 @@ os_type = platform.platform()
 if 'linux' in os_type.lower():
     os_type=f"{distro.name()} {distro.version()}"
 
+print(f"OS Type: {os_type}")
+
 def extract_code_cells(file_path):
     # Read the notebook
+    print(f"Reading notebook: {file_path}")
     with open(file_path, "r", encoding="utf-8") as f:
         notebook = nbformat.read(f, as_version=4)
 
@@ -54,24 +57,33 @@ def read_file(filename):
         with open(filename, "r") as f:
             return f.read()
     except Exception as e:
-        return str(e)
+        print(f"Error: {e}")
+        return f"Error: {e}"
 
 def get_cwd(a):
     print("Running getcwd")
     try:
         directory = os.getcwd()
+        print(f"Current directory: {directory}")
         return directory
     except Exception as e:
+        print(f"Error: {e}")
         return f"Error: {e}"
 # Useful for when you need to run powershell commands. Arg: command: str
-def run_command(command):
+def run_powershell(command):
     print(f"Running Command: {command}")
     try:
-        result = subprocess.run(command, shell=True, check=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        return f"Standard Output:\n{result.stdout}\n\nError Output:\n{result.stderr if result.stderr else 'None'}"
+        command = command.strip("'")
+        command = command.strip('"')
+        result = subprocess.run(['powershell', '-Command', command], 
+                                capture_output=True, text=True, check=True)
+        output = result.stdout.strip()
+        error = None
     except subprocess.CalledProcessError as e:
-        return f"Command failed with error:\nStandard Output:\n{e.stdout}\n\nError Output:\n{e.stderr if e.stderr else 'None'}"
-
+        output = e.stdout.strip()
+        error = e.stderr.strip()
+    print(f"Output: {output}\nError: {error if error else 'None'}")
+    return f"Output: {output}\nError: {error if error else 'None'}"
 
 
 # Collect all tools
@@ -93,7 +105,7 @@ tools = [
     ),
     Tool.from_function(
         name="Execute comands",
-        func=run_command,
+        func=run_powershell,
         description=f"Useful for when you need to run commands of {os_type} operating system, run codes, control the computer through Powershell CLI, etc. Run commands of {os_type} operating system only, nothing else. Arg: command: str"
     ),
     Tool.from_function(
